b95a26444d76ecc8a16618729c13dd1a4b2f0e6a
mohammad-inja <mohammad@inja.app>
2024-04-05 17:18:53 +0330

    feat(navbar): compelete navbar mobile and desktop

---
+++ components/shared/search/GlobalSearch.tsx
@@ -0,0 +1,26 @@
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import React from "react";
+
+const GlobalSearch = () => {
+  return (
+    <div className="relative w-full max-w-[600px] max-lg:hidden">
+      <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4 ">
+        <Image
+          src={"/assets/icons/search.svg"}
+          width={24}
+          height={24}
+          alt="search"
+        />
+        <Input
+          type="text"
+          value=""
+          placeholder="Search globally"
+          className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none "
+        />
+      </div>
+    </div>
+  );
+};
+
+export default GlobalSearch;
