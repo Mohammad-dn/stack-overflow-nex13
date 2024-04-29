2b5bde9ce6edb6b1a4b3f5a29dccbaaac95552fa
Mohammad-dn <mohammaddehghani2677@gmail.com>
2024-04-29 10:39:46 +0330

    add user model

---
+++ database/question.model.ts
@@ -0,0 +1,29 @@
+import { Schema, model, models, Document } from "mongoose";
+
+export interface IQuestions extends Document {
+  title: string;
+  content: string;
+  tags: Schema.Types.ObjectId[];
+  views: number;
+  upvotes: Schema.Types.ObjectId[];
+  downvotes: Schema.Types.ObjectId[];
+  author: Schema.Types.ObjectId[];
+  answers: Schema.Types.ObjectId[];
+  createdAt: Date;
+}
+
+const QuestionShema = new Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+  views: { type: Number, default: 0 },
+  upvots: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  downvots: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  author: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  answers: [{ type: Schema.Types.ObjectId, ref: "Answer" }],
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Question = models.Question || model("Question", QuestionShema);
+
+export default Question;
---
+++ database/user.model.ts
@@ -0,0 +1,35 @@
+import { Schema, model, models, Document } from "mongoose";
+
+export interface IUser extends Document {
+  clerkId: string;
+  name: string;
+  username: string;
+  email: string;
+  password?: string;
+  bio?: string;
+  picture: string;
+  location: string;
+  portfolioWebsite?: string;
+  reputation?: number;
+  saved: Schema.Types.ObjectId[];
+  joinedAt: Date;
+}
+
+const UserSchema = new Schema({
+  clerkId: { type: String, required: true },
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  bio: { type: String },
+  picture: { type: String, required: true },
+  location: { type: String },
+  portfolioWebsite: { type: String },
+  reputation: { type: Number },
+  saved: [{ type: Schema.Types.ObjectId, ref: "Question" }], // Assuming 'Saved' is another mongoose model
+  joinedAt: { type: Date, default: Date.now },
+});
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
