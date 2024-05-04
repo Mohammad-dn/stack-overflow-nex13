9de3a25466239e4d00a6656d1332b8e98d6e1715
Mohammad-dn <mohammaddehghani2677@gmail.com>
2024-05-04 17:12:15 +0330

    crud user and add webhooks

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,14 +2,65 @@
 
 import User from "@/database/user.model";
 import { connectedToDatabase } from "../mongoos";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  UpdateUserParams,
+} from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
 
-export async function getUserById(params: any) {
+export async function createUser(userData: CreateUserParams) {
   try {
     connectedToDatabase();
-    const { userId } = params;
-    const user = await User.find({ clerkId: userId });
+    const newUser = await User.create(userData);
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
 
-    return user;
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectedToDatabase();
+
+    const { clerkId, updateData, path } = params;
+    await User.findByIdAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectedToDatabase();
+
+    const { clerkId } = params;
+
+    const user = await User.findOneAndDelete({ clerkId });
+    if (!user) {
+      throw new Error("user not found");
+    }
+    // delete user from data base
+    // and question, answers, comments, etc
+    // get user question id
+
+    const questionUserIds = await Question.find({ author: user._id }).distinct(
+      "_id"
+    );
+
+    // delete question user
+
+    await Question.deleteMany({ author: user._id });
+
+    // Todo: delete users answers,comments and more
+
+    const deletedUser = await User.findByIdAndDelete(user._id);
+    return deletedUser;
   } catch (error) {
     console.log(error);
     throw error;
