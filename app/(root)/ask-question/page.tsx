c9223a44f0a6d0bb74259f5db17d8bc6d363cf8a
Mohammad-dn <mohammaddehghani2677@gmail.com>
2024-05-05 01:17:19 +0330

    fix deploy error

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 
 const Page = async () => {
   // const { userId } = auth();
-  const userId = "123abd";
+  const userId = "123abc";
 
   if (!userId) redirect("/sign-in");
   const mongoUser = await getUserById({ userId });
