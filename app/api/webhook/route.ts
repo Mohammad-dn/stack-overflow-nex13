eeabcc9638af99ea2d6ced5419e4f77dfe359088
Mohammad-dn <mohammaddehghani2677@gmail.com>
2024-05-07 13:46:26 +0330

    bug fix

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -82,7 +82,7 @@ export async function POST(req: Request) {
   if (eventType === "user.updated") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
-    console.log(evt.data);
+    console.log("evt.data", evt.data);
 
     const mongoUser = await updateUser({
       clerkId: id,
