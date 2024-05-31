0a6fd263925a1120276c2be6cdbf4988e89dc6ee
Mohammad-dn <mohammaddehghani2677@gmail.com>
2024-05-31 23:42:40 +0330

    fix question page

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -1,5 +1,7 @@
+import Answer from "@/components/forms/Answer";
 import Metric from "@/components/shared/Metric/Metric";
 import ParseHTML from "@/components/shared/ParseHTML";
+import RenderTag from "@/components/shared/RenderTag/RenderTag";
 import { getQuestionById } from "@/lib/actions/question.actions";
 import { formatAndDivideNumber, getTimeStamp } from "@/lib/utils";
 import Image from "next/image";
@@ -59,6 +61,18 @@ const page = async ({ params, searchParams }) => {
       </div>
 
       <ParseHTML data={result.content} />
+
+      <div className="mt-8 flex flex-wrap gap-2">
+        {result?.tags.map((tag: any) => (
+          <RenderTag
+            key={tag._id}
+            _id={tag._id}
+            name={tag.name}
+            showCount={false}
+          />
+        ))}
+      </div>
+      <Answer />
     </>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 // eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 import { ThemeProvider } from "@/context";
+import "./globals.css";
+import "../styles/prism.css";
 
 export const metadata: Metadata = {
   title: "JSM Dev Flow",
